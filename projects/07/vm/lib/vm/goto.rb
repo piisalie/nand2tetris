46b8dad35ad7ae850251837dd131364e1e290ff0
Paul Dawson <excidere@gmail.com>
2015-09-18 20:58:33 -0500

    Fix the broken push

--- projects/07/vm/lib/vm/goto.rb
+++ projects/07/vm/lib/vm/goto.rb
@@ -8,7 +8,7 @@ module VM
     end
 
     def write_to(runtime, options = { })
-      # runtime.push(1, as: "A") if !conditional
+      runtime.load_data(number: 1) if !conditional
       runtime.pop
       runtime.jump_to_label("NE", options.fetch(:params).fetch(:label_name))
     end
